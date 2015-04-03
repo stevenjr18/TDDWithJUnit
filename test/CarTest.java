cab3d3b6b6505094644d3b93f3a1f2b0e9b25454
Steven Schneider <stevenjr18@gmail.com>
2015-04-02 22:46:52 -0500

    Added test for deceleration.

--- test/CarTest.java
+++ test/CarTest.java
@@ -32,6 +32,13 @@ public class CarTest {
 	}
 
 	// Create and pass test for deceleration.
+	@Test 
+	public void canDecelerate(){
+		Car car1 = new Car("Red", 100);
+		car1.accelerate(75);
+		car1.decelerate(10);
+		assertThat(car1.getSpeed(), is(65));
+	}
 	
 	// Create and pass test for no deceleration below 0.
 
